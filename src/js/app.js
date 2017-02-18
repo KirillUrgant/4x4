7e7fd2e938776519345fe1c8737955ea89f9c6be
Kirill Urgant <kirillkvashonin@yandex.ru>
2017-02-18 16:02:43 +0300

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,37 @@
 $(function () {
     function animateBlock (block) {
         block.addClass("item-block_active");
+        addConsole("animation start");
+
         setTimeout(function() {
+            addConsole("animation end");
             var next = block.parent().next().find(".item-block");
 
             if (next.length) {
                 animateBlock(next)
             } else {
-                alert("Done!")
+                addConsole("end progress");
+                alert("Done!");
+                reset();
             }
-        }, 300);
+        }, 1000);
 
         return true;
     }
 
     function addConsole(text) {
-        $(".console").append("<p class='console__item'>"+text+"</p>")
+        $(".console").append("<p class='console__item'>~ "+text+"</p>")
+    }
+
+    function reset() {
+        $(".item-block").removeClass("item-block_active");
+        $(".button").text("start");
     }
 
     $(".button").on('click', function (e) {
         e.preventDefault();
         $(this).text("in progress...");
+        addConsole("start progress");
 
         animateBlock($(".col:first-child .item-block"));
     })
